adfbf8208d295d59d78094971d2f5eacd49ef068
D. Czechowski <3913085+czechtech@users.noreply.github.com>
2024-11-26 15:58:00 -0500

    Fewer listed requirements

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
-import setuptools
-setuptools.setup(     
+from setuptools import setup 
+
+setup(     
     name="gdrive_ipynb_import",
-    version="2024.11.26.02",
+    version="2024.11.26.03",
     python_requires=">=3.6",
     py_modules=["gdrive_ipynb_import"],
-    install_requires=['pydrive2','google','oauth2client','importlib','nbformat'],
+    install_requires=['pydrive2','google','oauth2client'],
 )
