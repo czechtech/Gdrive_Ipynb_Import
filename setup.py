0d477a21426e4e351f1d8ecdc1d4164cf8c49d77
D. Czechowski <3913085+czechtech@users.noreply.github.com>
2024-11-26 15:50:06 -0500

    Fixing setup.py
    This is a module, not a package.  Listed required packages.  Incremented version.

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 import setuptools
 setuptools.setup(     
-     name="sub-module",     
-     version="2024.11.26.01",
-     python_requires=">=3.6",   
-     packages=["gdrive_ipynb_import"],
+    name="gdrive_ipynb_import",
+    version="2024.11.26.02",
+    python_requires=">=3.6",
+    py_modules=["gdrive_ipynb_import"],
+    install_requires=['pydrive2','google','oauth2client','importlib','nbformat'],
 )
